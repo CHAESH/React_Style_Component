2a9443458a9ea72bc67f6050ff739602e101fd52
chaesh <csh1708@gmail.com>
2020-03-31 15:32:45 +0900

    Theming

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import styled, { createGlobalStyle, css } from "styled-components";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 createGlobalStyle`
   body{
@@ -8,34 +9,37 @@ createGlobalStyle`
   }
 `;
 
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
-`;
-
 const Container = styled.div`
   height: 100vh;
   width: 100%;
-  ${awesomeCard};
   background-color: pink;
 `;
+const Card = styled.div`
+  background-color: red;
+`;
 
-const Input = styled.input.attrs({
-  required: true
-})`
-  ${awesomeCard};
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
 `;
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Input placeholder="hellpo"></Input>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Form />
+        </Container>
+      </ThemeProvider>
     );
   }
 }
+
+const Form = () => (
+  <Card>
+    <Button>Heelo</Button>
+  </Card>
+);
 
 export default App;
